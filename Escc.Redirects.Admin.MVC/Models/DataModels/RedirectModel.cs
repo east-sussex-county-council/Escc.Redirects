41aec4c08d7e2febeb19013ab5d90e2388443c9d
Cai Lehwald <cai.lehwald@eastsussex.gov.uk>
2017-09-04 13:56:57 +0100

    Add 200 character limit to redirect comments using data annotations and jquery validate. update web.example.config to inlcude woff mimetypes

--- Escc.Redirects.Admin.MVC/Models/DataModels/RedirectModel.cs
+++ Escc.Redirects.Admin.MVC/Models/DataModels/RedirectModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Escc.Redirects.Admin.MVC.Models.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Escc.Redirects.Admin.MVC.Models.DataModels
 {
     // Model to hold basic data about a Redirect from the Redirects Database
     public class RedirectModel
@@ -6,6 +8,7 @@
         public int RedirectID { get; set; }
         public string Pattern { get; set; }
         public int Type { get; set; }
+        [MaxLength(200 ,ErrorMessage = "Comments can't be longer than 200 characters.")]
         public string Comment { get; set; }
         public string Destination { get; set; }
         public RedirectModel(int redirectID, string pattern, int type, string comment, string destination)
